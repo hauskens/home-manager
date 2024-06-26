e81a8b06a3029f21c5527983a0e6cb3304a47ff4
hauskens <hauskens-git@disp.lease>
2024-06-26 23:17:26 +0200

    formatting

--- modules/lib/maintainers.nix
+++ modules/lib/maintainers.nix
@@ -105,7 +105,8 @@
     email = "hauskens-git@disp.lease";
     github = "hauskens";
     githubId = 79340822;
-    keys = [ { fingerprint = "3582 5B85 66C8 4F36 45C7  EC42 809F 7938 9CB1 8650"; } ];
+    keys =
+      [{ fingerprint = "3582 5B85 66C8 4F36 45C7  EC42 809F 7938 9CB1 8650"; }];
   };
   henrisota = {
     email = "henrisota@users.noreply.github.com";
